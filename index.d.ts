6a3b27a237efde7ab27b3a38ae6ae16b41a50150
Eric <developer.ericgitangu@gmail.com>
2024-08-11 22:30:46 -0400

    Prisma migrations.

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-// global.d.ts
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  let prisma: PrismaClient | undefined;
-}
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+// Extend the global object to include the prisma property
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+// Use a conditional declaration to avoid redeclaring prisma
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
 export default prisma;
