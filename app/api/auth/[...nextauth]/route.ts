74bb9e53610f47e744f0ef5d9da23083089650ab
Eric <developer.ericgitangu@gmail.com>
2024-08-11 21:11:45 -0400

    Callback erro.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,27 @@
+// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../../lib/prisma"; // Update to the correct path where your prisma client is located
+import prisma from "@/lib/prisma"; // Adjust the path based on your project structure
 
 const handler = NextAuth({
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-	],
-	adapter: PrismaAdapter(prisma),
-	secret: process.env.NEXTAUTH_SECRET,
-	session: {
-		strategy: "jwt",
-	},
-	callbacks: {
-		async session({ session, token, user }) {
-			session.user!.email = user.email;
-			return session;
-		},
-	},
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user!.email = user.email;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
